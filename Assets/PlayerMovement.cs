d81c82d8095ed95cf8e34f31cabbc5616033dc94
Polalf <103451330+Polalf@users.noreply.github.com>
2024-01-05 17:01:37 -0300

    avance 05-01-24
    le puse el salto y el camera follow

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,18 +6,42 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float speed = 10;
     private Rigidbody rb;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 100f;
+    private bool canJump;
+    [SerializeField] private float distRayToGround = 2f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-   
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (canJump)
+            {
+                rb.AddForce(transform.up * jumpForce);
+            }
+        }
+    }
+
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         rb.position += new Vector3(x, 0, z).normalized * speed * Time.fixedDeltaTime;
+
+        #region Raycast
+        Ray ray = new Ray(transform.position, -transform.up);
+        canJump = Physics.Raycast(ray, distRayToGround);
+        Debug.DrawRay(transform.position, -transform.up*distRayToGround, Color.red);
+        #endregion 
     }
+
 }
