8148b03a737333a590a5d8debdc4717eabfbf17f
Polalf <103451330+Polalf@users.noreply.github.com>
2024-01-10 18:18:55 -0300

    avance en batalla

---
+++ Assets/Scripts/Battle/CharacterBattle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBattle : MonoBehaviour
+{
+    //Variables
+    [SerializeField] protected float life;
+
+    //Properties
+    public float Life => life;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void Death;
+}
